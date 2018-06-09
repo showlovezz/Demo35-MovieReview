e83351f2b01dc528338e98e51a9a14f7dc8bad18
vita <dragic.hsieh@gmail.com>
2018-06-09 18:48:43 +0800

    Done devise gem install and set

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,11 +7,11 @@ class MoviesController < ApplicationController
   end
 
   def new
-    @movie = Movie.new
+    @movie = current_user.movies.build
   end
 
   def create
-    @movie = Movie.new(movie_params)
+    @movie = current_user.movies.build(movie_params)
 
     if @movie.save
       redirect_to movies_path, notice: "電影新增成功"
